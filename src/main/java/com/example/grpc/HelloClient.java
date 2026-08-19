311d6d7cf03e34900c402531d331fac634a45aac
Jayakumar <jaykumar777@gmail.com>
2026-08-19 12:40:55 -0400

    test

--- src/main/java/com/example/grpc/HelloClient.java
+++ src/main/java/com/example/grpc/HelloClient.java
@@ -10,6 +10,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+//grpc 
 public class HelloClient {
 
     private static final String HOST = "localhost";
